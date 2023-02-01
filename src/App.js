634e877a918ee659c929837dbbf03bacf6c5965c
Jaison John <jaisonjohny26@gmail.com>
2023-02-01 18:27:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer/>
+      <h2>Hello react</h2>
+    </div>
+  );
+}
+
+export default App;
