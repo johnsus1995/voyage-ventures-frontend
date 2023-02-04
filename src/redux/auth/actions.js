71e403fab368a544b34811440d3d25a88435e673
Jaison John <jaisonjohny26@gmail.com>
2023-02-04 23:35:54 +0530

    tested toast with err message

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -2,15 +2,18 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as authApi from "../../api/auth";
 
 export const login = createAsyncThunk("auth/login", async (data) => {
-  // debugger;
-  console.log(data);
   const { formValues, navigate, toast } = data;
   try {
     const res = await authApi.login({
       email: formValues.email,
       password: formValues.password,
     });
+    if(res.status === 200){
+      navigate("/");
+      return
+    }
   } catch (error) {
     console.log(error);
+    toast(error.response.data.message)
   }
 });
