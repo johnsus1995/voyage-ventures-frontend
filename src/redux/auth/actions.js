9e7cf7b0b17a4cb0b3a53f299b321b5459bca7bb
Jaison John <jaisonjohny26@gmail.com>
2023-02-07 22:11:57 +0530

    register action modified

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -20,21 +20,19 @@ export const login = createAsyncThunk("auth/login", async (data) => {
 
 export const register = createAsyncThunk("auth/register", async (data) => {
   const { formValues, navigate, toast } = data;
-  try {
-    const res = await authApi.register({
-      first_name: formValues.first_name,
-      last_name: formValues.last_name,
-      email: formValues.email,
-      password: formValues.password,
-      confirm_password: formValues.confirm_password,
-    });
-    if (res.status === 201) {
-      navigate("/");
-      toast.success("Account created successfully.");
-      return res?.data;
-    }
-  } catch (error) {
-    console.log(error);
-    toast.error(error.response.data.message);
+  const res = await authApi.register({
+    first_name: formValues.first_name,
+    last_name: formValues.last_name,
+    email: formValues.email,
+    password: formValues.password,
+    confirm_password: formValues.confirm_password,
+  });
+  if (res.status === 201) {
+    navigate("/");
+    toast.success("Account created successfully.");
+    return res?.data;
+  } else {
+    console.log(res);
+    // toast.error(error.response.data.message);
   }
 });
